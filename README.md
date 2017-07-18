97bf619885103deb8287bdcd172cd3c9f8ec74b8
kodosenshi <kodomasuta@gmail.com>
2017-07-18 19:44:08 -0400

    update readme

--- README.md
+++ README.md
@@ -6,10 +6,36 @@
 
 In order to run this application locally, clone it to your projects directly and then use the installation instructions below.   
 
+```
+> cd /path/to/your/projects/folder
+> git clone https://github.com/kodosenshi/express-test-app.git
+```   
+
+For instance all my projects are in a **projects** directory in my home folder so I would do this:
+
+```
+> cd ~/projects
+> git clone https://github.com/kodosenshi/express-test-app.git
+```
+
+Once you have cloned this app locally you can `cd` into this project and complete the instructions below.   
+
+```
+> cd /path/to/your/projects/folder/express-test-app
+```
+
+So I would do this:    
+
+```
+> cd /path/to/your/projects/folder   
+> git clone https://github.com/kodosenshi/express-test-app.git   
+> cd express-test-app
+```   
+
 - - -
 
-### Install Nodemon
-* install Nodemon globally: This package will restart your server whenever there is a code change.
+### Install Nodemon (You only need to do this ONCE)
+* install Nodemon globally: This package will restart your server whenever there is a code change. So you don't need to kill the server and then start it again. Just code and voilà, automatic restarts. Note you only need to install Nodemon once since youa are installing it globally!!!! If you have it installed skip any part of the instruction where you install it again.
 
 ```
 > npm install -g nodemon
@@ -36,3 +62,25 @@ Now whenever you make a change to your file nodemon will restart the app for you
 ```
 > npm install
 ```
+
+## Start Web Server   
+
+```
+> nodemon index.js
+```
+
+So my complete instructions would be:
+
+```
+> cd ~/projects  
+> git clone https://github.com/kodosenshi/express-test-app.git   
+> cd express-test-app
+> npm install -g nodemon
+> npm install
+> nodemon index.js
+
+```   
+
+## View Site  
+
+Open your browser and go to: [http://localhost:3000](http://localhost:3000)
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// require Express, the framework we'll use for building
+// node based web apps.
 const express = require('express');
+
+// Remember node doesnt give use the body data from a POST request
+// we must use middleware for that, this package does that for us
 const parser = require('body-parser');
+
+// create a new app
 const app = express();
 
+// Tell our app we want to use the 'ejs' template rendering engine
 app.set('view engine', 'ejs');
 
+// store some simple data we'll pass to our templates as a test
 const classData = {
   title: 'PTWDI',
   students: [
@@ -12,22 +21,47 @@ const classData = {
   ]
 }
 
-//parses requests with the content type of `application/json`
+//parses requests with the content type of `application/json` by default
+// but since our form is using FormData instead of JSON we need to use the right parser
+// https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
 app.use(parser.urlencoded({ extended: false }));
 
-app.get('/', function(request, response) {
+// setup a route for our index page.
+// This is similar to going to http://cnn.com/
+// For our app the url is http://localhost:3000/
+app.get('/', (request, response) => {
+
+    // render our index.ejs template
+    // by default templates are looked for in the "views" directory
+    // and pass the object "classData" to your template
     response.render('index', classData);
 });
 
-app.get('/about', function(request, response) {
+// create a route for our about page
+// For our app the url is http://localhost:3000/about
+app.get('/about', (request, response) => {
+
+    // render our about.ejs template
+    // by default templates are looked for in the "views" directory
     response.render('about');
 });
 
-app.post('/submit', function(request, response) {
-    //if a json payload is posted to `/submit`,
-    //body-parser's json parser will parse it and
-    //attach it as `request.body`.
-    
+// create a route to submit our form
+// For our app the url is http://localhost:3000/submit
+// NOTE this is a POST not a GET there for "app.post"
+app.post('/submit', (request, response) => {
+
+    // the object "request.body"
+    // is available because of the body-parser middleware
+    // we required above.
+    const color = request.body.color;
+
+    // render the "order-success" template
+    // passing it some custom data
+    // We can imagine added someting to a cart
+    // on an e-commerce site like nike.com
+    // and made and order where we want to show
+    // the user a receipt
     response.render('order-success', {
         total: '400.50',
         color: request.body.color,
@@ -37,6 +71,11 @@ app.post('/submit', function(request, response) {
     });
 });
 
-const server = app.listen(4000, function () {
-   console.log('server started on port', server.address().port);
+// we try to start our webserver listening on PORT 3000
+// this means to see the webpage,
+// open your browser to http://localhost:3000
+// that will render the "/" route above
+
+const server = app.listen(port, () => {
+    console.log('server started on port', server.address().port);
 });
