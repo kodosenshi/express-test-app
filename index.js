a721dd0a779501d6f8a795dd76831b2989fcd5b6
kodosenshi <kodomasuta@gmail.com>
2017-07-26 16:53:38 -0400

    update example to use dynamic data for a super simple blog

--- index.js
+++ index.js
@@ -9,74 +9,74 @@ const parser = require('body-parser');
 // create a new app
 const app = express();
 
+app.use(express.static('public'));
+
 // Tell our app we want to use the 'ejs' template rendering engine
 app.set('view engine', 'ejs');
 
-// store some simple data we'll pass to our templates as a test
-const classData = {
-  title: 'PTWDI',
-  students: [
-    'pj',
-    'andre'
+// store some article data we'll pass to our template
+// eventually we'll use an external data source like a Database
+const blog = {
+  articles: [
+    {
+        title: 'About Shaun',
+        author: 'Shaun',
+        image: 'https://unsplash.it/960/504',
+        description: `Sed ut unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam<p>eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`,
+        link: '/about',
+        body: `
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        `,
+    },
+    {
+        title: 'My Schedule',
+        author: 'Shaun',
+        image: 'https://unsplash.it/960/503',
+        description: `Sed ut unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam<p>eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`,
+        link: '/schedule',
+        body: `
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+            <p>Sed ut unde omnis iste natus error sit voluptatem 
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+            ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        `
+    }
   ]
 }
 
-// the body parser middleware parses requests with the content type of `application/json` by default
-// but since our form is using FormData instead of JSON we need to use the right parser
-// https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
-// https://www.npmjs.com/package/body-parser#bodyparserurlencodedoptions
 app.use(parser.urlencoded({ extended: false }));
 
-// setup a route for our index page.
-// This is similar to going to http://cnn.com/
-// For our app the url is http://localhost:3000/
 app.get('/', (request, response) => {
-
-    // render our index.ejs template
-    // by default templates are looked for in the "views" directory
-    // and pass the object "classData" to your template
-    response.render('index', classData);
+    response.render('index', blog) ;
 });
 
-// create a route for our about page
-// For our app the url is http://localhost:3000/about
-app.get('/about', (request, response) => {
-
-    // render our about.ejs template
-    // by default templates are looked for in the "views" directory
-    response.render('about');
-});
-
-// create a route to submit our form
-// For our app the url is http://localhost:3000/submit
-// NOTE this is a POST not a GET therefor "app.post"
-app.post('/submit', (request, response) => {
-
-    // the object "request.body"
-    // is available because of the body-parser middleware
-    // we required above.
-    const color = request.body.color;
-
-    // render the "order-success" template
-    // passing it some custom data
-    // We can imagine added someting to a cart
-    // on an e-commerce site like nike.com
-    // and made and order where we want to show
-    // the user a receipt
-    response.render('order-success', {
-        total: '400.50',
-        color: request.body.color,
-        trackingInfo: {
-            type: 'FEDEX'
-        }
+blog.articles.forEach((article) => {
+    app.get(article.link, (request, response) => {
+        response.render('blog-detail', {
+            article: article, 
+            links: blog.articles.map((article) => {
+                return { 
+                    title: article.title, 
+                    link: article.link
+                }
+            })
+        });
     });
-});
+})
 
-// we try to start our webserver listening on PORT 3000
-// this means to see the webpage,
-// open your browser to http://localhost:3000
-// that will render the "/" route above
-
-const server = app.listen(3000, () => {
-    console.log('server started on port', server.address().port);
+const server = app.listen(8080, () => {
+    console.log('started')
 });
