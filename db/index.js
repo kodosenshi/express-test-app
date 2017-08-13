ef9b7259f7ee6c4579a151d1dea696ffbe62b761
kodosenshi <kodomasuta@gmail.com>
2017-08-13 11:05:18 -0400

    change db url name in condition

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 var sequelize;
 
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+if (process.env.DATABASE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
