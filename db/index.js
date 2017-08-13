b4d3d45c719bb62861048fc0cc05b3d3ab1170f9
kodosenshi <kodomasuta@gmail.com>
2017-08-13 10:55:28 -0400

    change pgconfig

--- db/index.js
+++ db/index.js
@@ -5,10 +5,7 @@ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
   // the application is executed on Heroku ... use the postgres database
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  true //false
+    protocol: 'postgres'
   })
 } else {
   // the application is executed on the local machine ... use mysql
